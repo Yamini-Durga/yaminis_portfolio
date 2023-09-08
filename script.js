55e5cc4a73352d7373eb6b77bcda5a0278ff0c0f
Yamini-Durga <Kodamanchiliyamini@gmail.com>
2023-09-08 22:08:25 +0530

    home and about me sections

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+// hamburger toggling
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+}
+// tab content changes in about me section
+var tablinks = document.querySelectorAll('.tab-links');
+var tabcontents = document.querySelectorAll('.tab-contents');
+
+function opentab(tabName){
+    for(var tablink of tablinks)
+    {
+        tablink.classList.remove("active-link");
+    }
+    for(var tabcontent of tabcontents)
+    {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabName).classList.add("active-tab");
 }
